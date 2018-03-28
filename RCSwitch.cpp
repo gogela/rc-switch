a60cc5dbe875790baa115ba7c70f6f5702f16292
gogela <gogela@atlas.cz>
2018-03-28 16:33:31 +0200

    Update RCSwitch.cpp
    added conrad meteo and niceflo sequences

--- RCSwitch.cpp
+++ RCSwitch.cpp
@@ -79,7 +79,9 @@ static const RCSwitch::Protocol PROGMEM proto[] = {
   { 380, {  1,  6 }, {  1,  3 }, {  3,  1 }, false },    // protocol 4
   { 500, {  6, 14 }, {  1,  2 }, {  2,  1 }, false },    // protocol 5
   { 450, { 23,  1 }, {  1,  2 }, {  2,  1 }, true },      // protocol 6 (HT6P20B)
-  { 150, {  2, 62 }, {  1,  6 }, {  6,  1 }, false }     // protocol 7 (HS2303-PT, i. e. used in AUKEY Remote)
+  { 150, {  2, 62 }, {  1,  6 }, {  6,  1 }, false },     // protocol 7 (HS2303-PT, i. e. used in AUKEY Remote)
+  { 350, {  1, 31 }, {  1,  3 }, {  3,  1 }, false },     // experimental - conrad meteo
+  {  90, {  16,17 }, {  5, 12 }, {  11, 6 }, false }      // experimental niceflo key - the sync should be {16,205,16,17}, further rec modification needed
 };
 
 enum {
